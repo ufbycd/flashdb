ba191fbe0e8cc0f2a43f84330f2b707f6e5f547e
chenss <ufbycd@163.com>
2012-09-14 23:43:01 +0800

    Init on locale

---
+++ src/flashdb.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : flashdb.c
+ Author      : chenss
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	puts("!!!Hello ARM World!!!"); /* prints !!!Hello ARM World!!! */
+
+
+	return EXIT_SUCCESS;
+}
